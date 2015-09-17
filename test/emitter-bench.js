fbb2e2c47b8df5d74dc088010939da0bc552b14d
Sam Eubank <sameubank+zerious@gmail.com>
2015-09-17 08:23:53 -0700

    Initial commit

---
+++ test/emitter-bench.js
@@ -0,0 +1,16 @@
+var fs = require('fs')
+var emitter = require(__dirname + '/lighter-emitter')
+
+describe('Benchmarks', function () {
+
+  bench('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
---
+++ test/emitter-test.js
@@ -0,0 +1,26 @@
+var emitter = require('../lighter-emitter')
+var is = global.is || require('exam/lib/is')
+
+describe('emitter', function () {
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+  describe('', function () {
+
+    it('', function () {
+    })
+
+    it('', function () {
+    })
+
+  })
+
+})
