1cad9b8089517a47396ce0930d56079cf2928dfe
Sam Eubank <sameubank+zerious@gmail.com>
2015-09-18 22:32:27 -0700

    Emitter benchmarks

--- lighter-emitter.js
+++ lighter-emitter.js
@@ -16,31 +16,34 @@ var Type = require('lighter-type')
 var Emitter = module.exports = Type.extend({
 
   /**
+   * Create a new event emitter.
+   */
+  init: function () {
+  },
+
+  /**
    * Set the maximum number of listeners that can listen to any type of event.
    */
   setMaxListeners: function (max) {
-    var self = this
-    self._maxListeners = max ? max : Infinity
-    return self
+    this._maxListeners = max ? max : Infinity
+    return this
   },
 
   /**
    * Handle the case of max listeners being exceeded for an event type.
    */
   maxListenersExceeded: function (type) {
-    var self = this
-    var max = self._maxListeners || Emitter.defaultMaxListeners
-    throw new Error('Max ' + max + ' listeners exceeded for "' + type + '".')
+    var max = this._maxListeners || Emitter.defaultMaxListeners
+    throw new Error('Max ' + this._maxListeners + ' listeners exceeded for "' + type + '".')
   },
 
   /**
    * Bind a function as a listener for a type of event.
    */
   on: function (type, fn) {
-    var self = this
-    var events = self._events = self._events || {}
+    var events = this._events = this._events || {}
     var listeners = events[type]
-    var max = self._maxListeners || Emitter.defaultMaxListeners
+    var max = this._maxListeners || Emitter.defaultMaxListeners
     // If there's only one, don't waste an Array.
     if (!listeners) {
       events[type] = fn
@@ -50,7 +53,7 @@ var Emitter = module.exports = Type.extend({
       if (max > 1) {
         events[type] = [listeners, fn]
       } else {
-        self.maxListenersExceeded(type)
+        this.maxListenersExceeded(type)
       }
 
     // When it's already an Array, push unless we've exceeded the max.
@@ -58,31 +61,29 @@ var Emitter = module.exports = Type.extend({
       if (listeners.length < max) {
         listeners.push(fn)
       } else {
-        self.maxListenersExceeded(type)
+        this.maxListenersExceeded(type)
       }
     }
-    return self
+    return this
   },
 
   /**
    * Set an event listener to be fired only once.
    */
   once: function (type, fn) {
-    var self = this
     function one () {
-      self.removeListener(type, one)
-      fn.apply(self, arguments)
+      this.removeListener(type, one)
+      fn.apply(this, arguments)
     }
-    self.on(type, one)
-    return self
+    this.on(type, one)
+    return this
   },
 
   /**
    * Emit an event with optional data.
    */
   emit: function (type, data) {
-    var self = this
-    var events = self._events
+    var events = this._events
     if (events) {
       var listeners = events[type]
       if (listeners) {
@@ -94,32 +95,31 @@ var Emitter = module.exports = Type.extend({
         // If there's only one listener, run it.
         if (typeof listeners === 'function') {
           if (args) {
-            listeners.apply(self, args)
+            listeners.apply(this, args)
           } else {
-            listeners.call(self, data)
+            listeners.call(this, data)
           }
 
         // If there's more than one listener, run them all.
         } else {
           for (var i = 0, l = listeners.length; i < l; i++) {
             if (args) {
-              listeners[i].apply(self, args)
+              listeners[i].apply(this, args)
             } else {
-              listeners[i].call(self, data)
+              listeners[i].call(this, data)
             }
           }
         }
       }
     }
-    return self
+    return this
   },
 
   /**
    * Return an array of listeners for an event type.
    */
   listeners: function (type) {
-    var self = this
-    var events = self._events
+    var events = this._events
     var list = events ? events[type] : undefined
     return !list ? [] : list instanceof Array ? list : [list]
   },
@@ -128,54 +128,51 @@ var Emitter = module.exports = Type.extend({
    * Remove an event listener.
    */
   removeListener: function (type, fn) {
-    var self = this
-    var events = self._events
+    var events = this._events
     if (events) {
       var listeners = events[type]
       if (listeners === fn) {
         delete events[type]
-        self.emit('removeListener', type, fn)
+        this.emit('removeListener', type, fn)
       } else if (listeners instanceof Array) {
         for (var i = listeners.length - 1; i >= 0; i--) {
           if (listeners[i] === fn) {
             listeners.splice(i, 1)
-            self.emit('removeListener', type, fn)
+            this.emit('removeListener', type, fn)
           }
           if (listeners.length === 1) {
             listeners = listeners[0]
-            return self
+            return this
           }
         }
       }
     }
-    return self
+    return this
   },
 
   /**
    * Remove all event listeners (optionally of a specified type).
    */
   removeAllListeners: function (type) {
-    var self = this
-    var events = self._events
+    var events = this._events
     // We only need to do something if there are events.
     if (events) {
       if (type) {
         delete events[type]
       } else {
-        delete self._events
+        delete this._events
       }
     }
-    return self
+    return this
   },
 
   /**
    * Set one listener for a type of event (replacing any others).
    */
   one: function (type, fn) {
-    var self = this
-    var events = self._events = self._events || {}
+    var events = this._events = this._events || {}
     events[type] = fn
-    return self
+    return this
   }
 
 })
--- test/emitter-bench.js
+++ test/emitter-bench.js
@@ -1,14 +1,79 @@
-var fs = require('fs')
-var emitter = require('../lighter-emitter')
+var Emitter = require('../lighter-emitter')
+var EventEmitter = require('events').EventEmitter
 
 describe('Benchmarks', function () {
+  this.timeout(1e4)
 
-  bench('', function () {
+  bench('Instantiation', function () {
+    this.sampleSize = 1e3
+    this.minimumSamples = 1e3
 
-    it('', function () {
+    it('lighter-emitter', function () {
+      new Emitter()
     })
 
-    it('', function () {
+    it('events.EventEmitter', function () {
+      new EventEmitter()
+    })
+
+  })
+
+  bench('Listening', function () {
+    this.sampleSize = 1e3
+    this.minimumSamples = 1e3
+
+    var e = new Emitter()
+    e.setMaxListeners(Infinity)
+
+    var ee = new EventEmitter()
+    ee.setMaxListeners(Infinity)
+
+    it('lighter-emitter', function () {
+      e.on('a', function () {})
+    })
+
+    it('events.EventEmitter', function () {
+      ee.on('a', function () {})
+    })
+
+  })
+
+  bench('Emitting', function () {
+    this.sampleSize = 1e4
+    this.minimumSamples = 1e3
+
+    var e = new Emitter()
+    e.on('a', function () {})
+
+    var ee = new EventEmitter()
+    ee.on('a', function () {})
+
+    it('lighter-emitter', function () {
+      e.emit('a', 1)
+    })
+
+    it('events.EventEmitter', function () {
+      ee.emit('a', 1)
+    })
+
+  })
+
+  bench('Listening Once', function () {
+    this.sampleSize = 1e3
+    this.minimumSamples = 1e3
+
+    var e = new Emitter()
+    var ee = new EventEmitter()
+    var f = function () {}
+
+    it('lighter-emitter', function () {
+      e.once('a', f)
+      e.emit('a', 1)
+    })
+
+    it('events.EventEmitter', function () {
+      ee.once('a', f)
+      ee.emit('a', 1)
     })
 
   })
